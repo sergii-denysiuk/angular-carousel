eb6abe5c506866c48de50eb7e1db21565455af16
Sergii Denysiuk <sergii.denysiuk@gmail.com>
2017-04-06 01:31:44 +0200

    restart automatic slideshow, when click  or  button

--- js/carousel.js
+++ js/carousel.js
@@ -31,33 +31,66 @@
             },
             templateUrl: 'templates/carousel.html',
             link: function(scope, elem, attrs) {
-                var slideTime = scope.slideTime || 3000;
+                var slideTime = scope.slideTime || 5000;
+                var timer;
 
                 scope.currentIndex = 0;
+                scope.clickNext = clickNext;
+                scope.clickPrev = clickPrev;
 
-                scope.next = function() {
+                activate();
+
+                function activate() {
+                    startTimer();
+                }
+
+                function clickNext() {
+                    stopTimer();
+                    next();
+                    startTimer();
+                }
+
+                function clickPrev() {
+                    stopTimer();
+                    prev();
+                    startTimer();
+                }
+
+                function next() {
                     scope.images[scope.currentIndex].visible = false;
                     scope.currentIndex < scope.images.length - 1 ?
                         scope.currentIndex++ : scope.currentIndex = 0;
                     scope.images[scope.currentIndex].visible = true;
-                };
 
-                scope.prev = function() {
+                }
+
+                function prev() {
                     scope.images[scope.currentIndex].visible = false;
                     scope.currentIndex > 0 ?
                         scope.currentIndex-- : scope.currentIndex = scope.images.length - 1;
                     scope.images[scope.currentIndex].visible = true;
-                };
 
-                /* Start: For Automatic slideshow*/
-                var timer = $interval(function() {
-                    scope.next();
-                }, slideTime);
+                }
+
+                /**
+                 * Start for Automatic slideshow
+                 */
+                function startTimer() {
+                    timer = $interval(function() {
+                        next();
+                    }, slideTime);
+                }
+
+                /**
+                 * End for Automatic slideshow
+                 */
+                function stopTimer() {
+                    $interval.cancel(timer);
+                }
 
                 scope.$on('$destroy', function() {
-                    $interval.cancel(timer);
+                    stopTimer();
                 });
-                /* End : For Automatic slideshow*/
             }
         };
     }
