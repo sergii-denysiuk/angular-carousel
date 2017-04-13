ee6da967709dbe5c81863a24fa32d692b1de9cdb
Sergii Denysiuk <sergii.denysiuk@gmail.com>
2017-04-13 23:22:21 +0200

    separate carousel source-code from carousel test-code

--- js/carousel.js
+++ js/carousel.js
@@ -1,23 +1,8 @@
 (function() {
     'use strict';
 
-    angular.module('carouselApp', [
-            'ngAnimate'
-        ])
-        .controller('CarouselController', CarouselController)
+    angular.module('carouselApp')
         .directive('carousel', carousel);
-
-    CarouselController.$inject = ['$scope'];
-
-    function CarouselController($scope) {
-        $scope.images = [
-            { src: 'img/img1.png', title: 'Pic 1' },
-            { src: 'img/img2.jpg', title: 'Pic 2' },
-            { src: 'img/img3.jpg', title: 'Pic 3' },
-            { src: 'img/img4.png', title: 'Pic 4' },
-            { src: 'img/img5.png', title: 'Pic 5' }
-        ];
-    }
 
     carousel.$inject = ['$interval'];
 
